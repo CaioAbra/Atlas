180d22af161a28398e5fbd8ea44250552a4aaa8a
CaioAbra <caioabra41@gmail.com>
2024-08-09 00:33:40 -0300

    ajuste de fonte accordion

--- script.js
+++ script.js
@@ -13,6 +13,8 @@ $('.accordion-header').click(function () {
     const target = $(this).data('target'); // Obtém o ID do item correspondente
     const $body = $(target);
     const $header = $(this); // Armazena o header clicado
+    const arowUp = `<img src="./src/img/arrow.svg" alt="" srcset="">`;
+    const arowDown = `<img src="./src/img/arrowDown.svg" alt="" srcset="">`;
 
     // Verifica se o corpo está aberto
     if ($body.hasClass('open')) {
@@ -20,19 +22,22 @@ $('.accordion-header').click(function () {
         $body.slideUp(300, function () {
             $body.removeClass('open'); // Remove a classe "open" do corpo
             $header.removeClass('open'); // Remove a classe "open" do header correspondente
-            $header.find('.icon').text('+'); // Muda ícone para +
+            // $header.find('.icon').text('+'); // Muda ícone para +
+            $header.find('.icon').html(arowDown); // Muda ícone para +
         });
     } else {
         // Se não estiver aberto, fecha todos os outros
         $('.accordion-body').slideUp(300).removeClass('open');
         $('.accordion-header').removeClass('open'); // Remove a classe "open" de todos os headers
-        $('.accordion-header .icon').text('+'); // Reseta todos os ícones para +
+        // $('.accordion-header .icon').text('+'); // Reseta todos os ícones para +
+        $('.accordion-header .icon').html(arowUp); // Reseta todos os ícones para +
 
         // Abre o corpo atual
         $body.slideDown(300, function () {
             $body.addClass('open'); // Adiciona a classe "open" ao corpo atual
             $header.addClass('open'); // Adiciona a classe "open" ao header correspondente
-            $header.find('.icon').text('-'); // Muda ícone para -
+            // $header.find('.icon').text('-'); // Muda ícone para -
+            $header.find('.icon').html(arowDown); // Muda ícone para -
         });
     }
 });
